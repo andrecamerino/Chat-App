4297aefcf853d4bf21db45608ee2e5bc1e53123d
andrecamerino <camerinoandre@gmail.com>
2024-12-05 18:45:21 +1300

    update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,18 @@
 import express from "express";
+import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 
-const PORT = 5001;
+dotenv.config()
+
+const PORT = process.env.PORT;
 const app = express();
+
+app.use(express.json())
 
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
+  connectDB();
 });
