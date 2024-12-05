25973a6fa8d313269a312848ef1162883cd21a45
andrecamerino <camerinoandre@gmail.com>
2024-12-05 20:13:33 +1300

    update auth routes

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,12 +1,24 @@
 import express from "express";
-import { signup, login, logout } from "../controllers/auth.controller.js";
+import {
+  signup,
+  login,
+  logout,
+  updateProfile,
+  checkAuth
+} from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post("/signup", signup)
+router.post("/signup", signup);
 
-router.post("/login", login)
+router.post("/login", login);
 
-router.get("/logout", logout)
+router.post("/logout", logout);
+
+router.get("/check", protectRoute, checkAuth)
+
+// protect route ensures auth, then allows update
+router.put("/update-profile", protectRoute, updateProfile);
 
 export default router;
